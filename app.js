6a4942d3a618f169c4251cf1c068c3098dd1aaf9
Max Chernikov <max.chernikoff@gmail.com>
2018-11-22 20:09:53 +0300

    Add AI to game :smile:

--- app.js
+++ app.js
@@ -5,38 +5,21 @@
     var board = new Array(9)
     var cells = document.getElementById('board').querySelectorAll('td')
     var gameOver = false
-
-    var generateRandomIndex = function() {
-        return Math.floor(Math.random() * 9)
-    }
-
-    var isBoardEmpty = function() {
-        var counter = 0
-
-        board.forEach(function(cell) {
-            if (cell) {
-                ++counter
-            }
-        })
-
-        return counter === board.length
+    var directions = {
+        up: [0, 1, 2],
+        bottom: [6, 7, 8],
+        left: [0, 3, 6],
+        right: [2, 5, 8],
+        diagLR: [0, 4, 8],
+        diagRL: [2, 4, 6],
+        vertical: [1, 4, 7],
+        horizontal: [3, 4, 5]
     }
 
     /**
      * @returns {Number} X is 1, 0 is 0, -1 is standoff
      */
     var checkRules = function() {
-        var directions = {
-            up: [0, 1, 2],
-            bottom: [6, 7, 8],
-            left: [0, 3, 6],
-            right: [2, 5, 8],
-            diagLR: [0, 4, 8],
-            diagRL: [2, 4, 6],
-            vertical: [1, 4, 7],
-            horizontal: [3, 4, 5]
-        }
-
         for (direction in directions) {
             var counterX = 0;
             var counter0 = 0;
@@ -59,8 +42,7 @@
         return -1
     }
 
-    var setMessage = function() {
-        var code = checkRules()
+    var showMessage = function(code) {
         var message = 'Standoff'
 
         if (code === 1) {
@@ -72,32 +54,74 @@
         document.getElementById('message').innerHTML = message
     }
 
+    /**
+     * @param {Number*} index Index of cell
+     * @param {String} symbol X or 0 symbol
+     */
+    var paintCell = function(index, symbol) {
+        board[index] = symbol
+        cells[index].innerHTML = symbol
+    }
+
     var playTurn = function() {
-        // Ход противника состоит из одного из вариантов:
-        // - проверить, что у противника непоследний ход, иначе заблокировать
-        // - найти свой символ и попробовать продолжить
-        // - или найти свободные трешки, поставить крестик
-        // - если нет продолжения, поставить рандомно (x)
+        // 1. проверить, что у противника непоследний ход, иначе заблокировать
+        for (name in directions) {
+            var indexes = directions[name]
+            var counter = 0
+            var hasEmptyCell = -1
 
-        if (isBoardEmpty()) {
-            gameOver = true
-            setMessage()
-        } else {
-            while(1) {
-                var index = generateRandomIndex()
-
-                if (!board[index]) {
-                    board[index] = '0'
-                    cells[index].innerHTML = '0'
-
-                    var result = checkRules()
-
-                    if (result === 0 || result === 1) {
-                        setMessage()
-                    }
-
-                    break
+            // count turns in all directions
+            indexes.forEach(function(index) {
+                if (board[index] === 'X') {
+                    ++counter
+                } else if (!board[index]) {
+                    hasEmptyCell = index
                 }
+            })
+
+            if (counter === 2 && hasEmptyCell !== -1) {
+                // заблокировать свободную клетку противнику
+                paintCell(hasEmptyCell, '0')
+                return
+            }
+        }
+
+        // 2. найти свой символ и попробовать продолжить
+        for (name in directions) {
+            var indexes = directions[name]
+
+            if (indexes[0] === 'X') {
+                if (!indexes[1]) {
+                    paintCell(1, 'X')
+                    return
+                } else if (!indexes[2]) {
+                    paintCell(2, 'X')
+                    return
+                }
+            } else if (indexes[1] === 'X') {
+                if (!indexes[0]) {
+                    paintCell(0, 'X')
+                    return
+                } else if (!indexes[2]) {
+                    paintCell(2, 'X')
+                    return
+                }
+            } else if (indexes[2] === 'X') {
+                if (!indexes[0]) {
+                    paintCell(0, 'X')
+                    return
+                } else if (!indexes[1]) {
+                    paintCell(1, 'X')
+                    return
+                }
+            }
+        }
+
+        // 3. или найти свободные трешки, поставить крестик
+        for (var i = 0; i < board.length; i++) {
+            if (!board[i]) {
+                paintCell(i, '0')
+                return
             }
         }
     }
@@ -105,14 +129,20 @@
     cells.forEach(function(cell) {
         cell.addEventListener('click', function() {
             if (!cell.innerHTML && gameOver === false) {
-                board[Number(cell.id) - 1] = 'X'
-                cell.innerHTML = 'X'
-
+                paintCell(Number(cell.id) - 1, 'X')
+    
                 var result = checkRules()
+    
                 if (result === -1) {
                     playTurn()
+    
+                    var result = checkRules()
+    
+                    if (result === 0 || result === 1) {
+                        showMessage(result)
+                    }
                 } else {
-                    setMessage()
+                    showMessage(result)
                     gameOver = true
                 }
             }
